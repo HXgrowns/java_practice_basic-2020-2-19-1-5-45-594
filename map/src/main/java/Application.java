e9293c296e07d37f08dadaa46a33ed79b3c799fa
huxiao <804156017@qq.com>
2020-02-20 12:07:54 +0800

    [huxiao] {add}: add map homework

--- map/src/main/java/Application.java
+++ map/src/main/java/Application.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
 
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(new Student("Linda", 18), new Student("Cindy", 19), new Student("Mary", 19));
+        Teacher teacher = new Teacher("Bob", students);
+        teacher.addStudent(new Student("Ben", 19));
+        teacher.addStudent(new Student("Solider", 20));
+        Set<Integer> studentAges = teacher.getAgeToStudents().keySet();
+        for (Integer studentAge : studentAges) {
+            System.out.println(studentAge + "岁 : " + teacher.getAgeToStudents().get(studentAge));
+        }
+    }
 
 }
--- map/src/main/java/Teacher.java
+++ map/src/main/java/Teacher.java
@@ -1,3 +1,30 @@
-public class Teacher {
+import java.util.*;
 
+public class Teacher {
+    private String name;
+    private Map<Integer, List<Student>> ageToStudents;
+
+    public Teacher(String name, List<Student> students) {
+        this.name = name;
+        this.ageToStudents = new HashMap<>();
+        for (Student student : students) {
+            if (student != null) {
+                List<Student> peerStudents = this.ageToStudents.getOrDefault(student.getAge(), new ArrayList<>());
+                peerStudents.add(student);
+                this.ageToStudents.put(student.getAge(), peerStudents);
+            }
+        }
+    }
+
+    public void addStudent(Student student) {
+        if (student != null) {
+            List<Student> students = this.ageToStudents.getOrDefault(student.getAge(), new ArrayList<>());
+            students.add(student);
+            this.ageToStudents.put(student.getAge(), students);
+        }
+    }
+
+    public Map<Integer, List<Student>> getAgeToStudents() {
+        return ageToStudents;
+    }
 }
